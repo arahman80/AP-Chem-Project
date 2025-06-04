73d36532373adf980c009749370ce047f16e6f62
Abdul Rahman <abdulrahman42908@gmail.com>
2025-06-04 14:10:09 -0400

    quick fix

--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,9 @@
 # create individual object files
-clang++ -include ../includes/chem.hpp -cpp ../src/chem/balance.cpp
-clang++ -include ../includes/chem.hpp -cpp ../src/chem/eq_eval.cpp
-clang++ -include ../includes/chem.hpp -cpp ../src/chem/format_expr.cpp
-clang++ -include ../includes/chem.hpp -cpp ../src/chem/parse.cpp
-clang++ -include ../includes/chem.hpp -cpp ../src/chem/therm_eval.cpp
+clang++ -o balance.o     -include ../includes/chem.hpp -cpp ../src/chem/balance.cpp
+clang++ -o eq_eval.o     -include ../includes/chem.hpp -cpp ../src/chem/eq_eval.cpp
+clang++ -o format_expr.o -include ../includes/chem.hpp -cpp ../src/chem/format_expr.cpp
+clang++ -o parse.o       -include ../includes/chem.hpp -cpp ../src/chem/parse.cpp
+clang++ -o therm_eval.o  -include ../includes/chem.hpp -cpp ../src/chem/therm_eval.cpp
 
 # link into final executable
 clang++ -o chemproj balance.o eq_eval.o format_expr.o parse.o therm_eval.o
