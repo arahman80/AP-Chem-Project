5116d1a1f20d667bcc8b4453f985181bd76b437c
Abdul Rahman <abdulrahman42908@gmail.com>
2025-06-04 13:52:20 -0400

    chemistry function implementations separated from primary c++ file

---
+++ chem.cpp
@@ -0,0 +1,21 @@
+#include <chem.hpp>
+
+std::vector<Compound>
+parse (std::string expr)
+{}
+
+std::vector<Compound>
+balance (std::vector<Compound> expr)
+{}
+
+int
+therm_eval (std::vector<Compound> expr)
+{}
+
+equilexpr
+eq_eval (std::vector<Compound> expr)
+{}
+
+void
+format_expr (std::vector<Compound> expr)
+{}
