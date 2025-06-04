f823682fe1336540bdf689bd86f563170786b8a9
Abdul Rahman <abdulrahman42908@gmail.com>
2025-06-04 13:52:51 -0400

    more focused main C++ file
    Consider giving each function its own header?

--- main.cpp
+++ main.cpp
@@ -1,34 +1,6 @@
 #include <iostream>
-#include <vector>
-#include <string>
+#include "chem.hpp"
 
-/* 
- * function prototype descriptions:
- *
- * std::vector<Coumpounnd> parse(std::string expr)
- * meant to scan and parse an inputted chemical equation to a machine readable format
- *
- * std::vector<Compound> balance(std::vector<Compound> expr)
- * takes a chemical expression and produces a balanced chemical equation (unit 1)
- *
- * int therm_eval(std::vector<Compound> expr) 
- * takes a chemical expression and returns the enthalpy (unit 6)
- *
- * equilexppr eq_eval(std::vector<Compound> expr)
- * takes a chemical equation and returns an equilibrium expression for it (unit 7)
- *
- * void format_expr(std::vector<Compound> expr)
- * prints out a formatted chemical expression
- */
-
-/* function prototypes */
-std::vector<Compound> parse       (std::string           expr);
-std::vector<Compound> balance     (std::vector<Compound> expr);
-int                   therm_eval  (std::vector<Compound> expr);
-equilexpr             eq_eval     (std::vector<Compound> expr);
-void                  format_expr (std::vector<Compound> expr);
-
-/* entry point (idk why i'm labelling this) */
 int
 main()
 {
@@ -39,24 +11,3 @@ main()
    * - show the equilibrium constant expression
    */
 }
-
-/* function implementations */
-std::vector<Compound>
-parse (std::string expr)
-{}
-
-std::vector<Compound>
-balance (std::vector<Compound> expr)
-{}
-
-int
-therm_eval (std::vector<Compound> expr)
-{}
-
-equilexpr
-eq_eval (std::vector<Compound> expr)
-{}
-
-void
-format_expr (std::vector<Compound> expr)
-{}
